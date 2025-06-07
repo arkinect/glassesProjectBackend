2b44b09c73ddf6ba6f501e9dc0aff406d2af2d13
mtplane03@gmail.com <mlane44@uwo.ca>
2025-06-06 23:42:02 -0400

    BB15 update db tables, also deleted all post data at the same time but oh well

--- alembic/env.py
+++ alembic/env.py
@@ -6,15 +6,14 @@ from sqlalchemy import pool
 from alembic import context
 from dotenv import load_dotenv
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'FastAPI')))
 from FastAPI.app.database import Base
+from FastAPI.app import models 
 
 load_dotenv()   
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
@@ -23,16 +22,7 @@ if db_url is None:
     raise RuntimeError("MYSQL_URL environment variable not set.")
 config.set_main_option("sqlalchemy.url", db_url)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
