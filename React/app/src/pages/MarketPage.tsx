3c7c3fce020a2040425debf8a36e3f0035baf3fe
mtplane03@gmail.com <mlane44@uwo.ca>
2025-08-07 11:06:05 -0400

    FB8 update market page to remove bug checking, update listing page to accomadate single item return

--- React/app/src/pages/MarketPage.tsx
+++ React/app/src/pages/MarketPage.tsx
@@ -40,10 +40,10 @@ const MarketPage: React.FC<props> = ({}) => {
             ): (
               <div className={"grid_container"}>
                 {postings.map((glasses: Posting, index) => {
-                   console.log('Image name for post', glasses);
                   return(
-                  <MarketTile postNumb={glasses.postNumb} location={glasses.location} sphere={glasses.sphere} coverImage={glasses.imageCard} key={index}></MarketTile>
-                )})}   
+                    <MarketTile postNumb={glasses.postNumb} location={glasses.location} sphere={glasses.sphere} coverImage={glasses.imageCard} key={index}></MarketTile>
+                  )
+                })}   
               </div>  
             )}
           </div>
